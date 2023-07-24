10302e9cc7766f0d48a20d8b506a7723c5465160
vitorGaia <vitorgaia0027@gmail.com>
2023-07-24 13:10:29 -0300

    fix: concerta erro ao chamar hook no componente ProductCard, agora a função addProductToCart não é mais chamada diretamente do contexto no componente e sim passada por props.

--- src/app/components/ProductCard/page.js
+++ src/app/components/ProductCard/page.js
@@ -4,16 +4,18 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React, { useContext } from 'react';
 
-function ProductCard({ product }) {
-  const { addProductToCart } = useContext(AppContext);
+function ProductCard({ product, addProductToCart }) {
+
   return (
     <div key={product.id} className='overflow-x-hidden flex flex-col justify-center gap-1 w-72 max-h-96 p-3 text-font bg-baseM shadow-md rounded-md relative overflow-auto hover:transform hover:scale-105 transition'>
+
       {(product.shipping && product.shipping.free_shipping)
       && (
         <div className='diagonal-div bg-yellow-400 absolute top-5 -right-10 h-7 w-36 flex justify-center items-center shadow-lg'>
           <p className='text-sm font-medium lg:font-semibold text-baseM'>Frete Grátis</p>
         </div>
       )}
+
       <Link href={`/pages/${product.id}`}>
         <Image
           src={product.thumbnail}
@@ -24,15 +26,18 @@ function ProductCard({ product }) {
           blurDataURL={product.thumbnail}
           className='w-11/12 pl-5 rounded-md'
         />
+
         <div className='h-6 mt-1 overflow-hidden'>
           <p className='font-medium truncate px-6 text-lg'>
             {product.title}
           </p>
         </div>
       </Link>
+
       <p className='text-lg font-semibold text-center'>
         {`R$ ${product.price.toFixed(2)}`}
       </p>
+
       <button
         onClick={() => addProductToCart(product)}
         className='bg-accent tracking-wide font-bold text-lg w-full h-12 mt-1 py-0.5 leading-4 rounded shadow-md px-1 text-baseM hover:bg-accentHover transition'
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 import './globals.css';
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import ProductCard from "./components/ProductCard/page";
 import { AppContext } from "./contexts/AppContext";
 import CategoriesList from './components/CategoriesList/page';
@@ -8,7 +8,7 @@ import Header from './components/Header/page';
 import Loading from './components/Loading/page';
 
 export default function Home() {
-  const { globalState, loading, activeSearch, handleFilters, showFilter, setShowFilter } = useContext(AppContext);
+  const { globalState, loading, activeSearch, handleFilters, showFilter, setShowFilter, addProductToCart } = useContext(AppContext);
   const { homeProducts } = globalState;
 
   useEffect(() => {
@@ -22,7 +22,7 @@ export default function Home() {
     window.addEventListener('scroll', handleScroll);
   }, [setShowFilter]);
 
-  const mapHomeProducts = homeProducts && homeProducts.map((product) => ProductCard({ product }));
+  const mapHomeProducts = homeProducts && homeProducts.map((product) => ProductCard({ product, addProductToCart }));
 
   const filters = (
     <select
@@ -58,14 +58,17 @@ export default function Home() {
         {showFilter && filters}
 
         {(!homeProducts && !loading)
-        && (<div className='flex flex-col items-center lg:py-40 lg:gap-6'>
-          <p className='hidden lg:flex text-4xl text-accent font-semibold text-center tracking-wide'>
-            VOCÊ AINDA NÃO<br/> REALIZOU UMA BUSCA
-          </p>
-          <h3 className="text-center py-56 text-2xl font-medium text-fontM lg:py-0">
-            Digite algum termo de pesquisa<br/> ou escolha uma categoria :)
-          </h3>
-        </div>)}
+        && (
+          <div className='flex flex-col items-center lg:py-40 lg:gap-6'>
+            <p className='hidden lg:flex text-4xl text-accent font-semibold text-center tracking-wide'>
+              VOCÊ AINDA NÃO<br/> REALIZOU UMA BUSCA
+            </p>
+
+            <h3 className="text-center py-56 text-2xl font-medium text-fontM lg:py-0">
+              Digite algum termo de pesquisa<br/> ou escolha uma categoria :)
+            </h3>
+          </div>
+        )}
 
         {(homeProducts && homeProducts.length === 0 && !loading)
         && (<h3 className="text-center text-2xl font-medium py-56 text-fontM">Nenhum produto foi encontrado ;(</h3>)}
